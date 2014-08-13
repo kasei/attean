98426deec1eb7d1d5753a4e1eaa131fb24b947e7
Gregory Todd Williams <greg@evilfunhouse.com>
2014-08-13 00:07:18 -0700

    Move ntriples_string builder default implementation into new RDF::API::IRI role.

--- lib/RDF/API/Term.pm
+++ lib/RDF/API/Term.pm
@@ -21,4 +21,52 @@ package RDF::API::Literal 0.001 {
 	has 'datatype'			=> (is => 'ro', isa => 'IRI', required => 1, coerce => 1, default => sub { IRI->new(value => 'http://www.w3.org/2001/XMLSchema#string') });
 }
 
+package RDF::API::IRI 0.001 {
+	use Moose::Role;
+	use IRI;
+	use Moose::Util::TypeConstraints;
+	
+	with 'RDF::API::Term';
+	
+	sub _ntriples_string {
+		my $self	= shift;
+		my $iri		= $self->value;
+		my @chars	= split(//, $iri);
+		my $string	= '';
+		while (scalar(@chars)) {
+			my $c	= shift(@chars);
+			my $o	= ord($c);
+			if ($o < 0x8) {
+				$string	.= sprintf("\\u%04X", $o);
+			} elsif ($o == 0x9) {
+				$string	.= "\\t";
+			} elsif ($o == 0xA) {
+				$string	.= "\\n";
+			} elsif ($o < 0xC) {
+				$string	.= sprintf("\\u%04X", $o);
+			} elsif ($o == 0xD) {
+				$string	.= "\\r";
+			} elsif ($o < 0x1F) {
+				$string	.= sprintf("\\u%04X", $o);
+			} elsif ($o < 0x21) {
+				$string	.= $c;
+			} elsif ($o == 0x22) {
+				$string	.= "\"";
+			} elsif ($o < 0x5B) {
+				$string	.= $c;
+			} elsif ($o == 0x5C) {
+				$string	.= "\\";
+			} elsif ($o < 0x7E) {
+				$string	.= $c;
+			} elsif ($o < 0xFFFF) {
+				$string	.= sprintf("\\u%04X", $o);
+			} else {
+				$string	.= sprintf("\\U%08X", $o);
+			}
+		}
+		my $ntriples	= '<' . $string . '>';
+		return $ntriples;
+	}
+}
+
 1;
--- lib/RDF/IRI.pm
+++ lib/RDF/IRI.pm
@@ -9,7 +9,7 @@ package RDF::IRI 0.001 {
 	
 	has 'ntriples_string'	=> (is => 'ro', isa => 'Str', lazy => 1, builder => '_ntriples_string');
 
-	with 'RDF::API::Term';
+	with 'RDF::API::IRI';
 	with 'RDF::BlankOrIRI';
 
 	around BUILDARGS => sub {
@@ -24,46 +24,6 @@ package RDF::IRI 0.001 {
 	sub as_string {
 		my $self	= shift;
 		return $self->abs;
-	}
-	
-	sub _ntriples_string {
-		my $self	= shift;
-		my $iri		= $self->value;
-		my @chars	= split(//, $iri);
-		my $string	= '';
-		while (scalar(@chars)) {
-			my $c	= shift(@chars);
-			my $o	= ord($c);
-			if ($o < 0x8) {
-				$string	.= sprintf("\\u%04X", $o);
-			} elsif ($o == 0x9) {
-				$string	.= "\\t";
-			} elsif ($o == 0xA) {
-				$string	.= "\\n";
-			} elsif ($o < 0xC) {
-				$string	.= sprintf("\\u%04X", $o);
-			} elsif ($o == 0xD) {
-				$string	.= "\\r";
-			} elsif ($o < 0x1F) {
-				$string	.= sprintf("\\u%04X", $o);
-			} elsif ($o < 0x21) {
-				$string	.= $c;
-			} elsif ($o == 0x22) {
-				$string	.= "\"";
-			} elsif ($o < 0x5B) {
-				$string	.= $c;
-			} elsif ($o == 0x5C) {
-				$string	.= "\\";
-			} elsif ($o < 0x7E) {
-				$string	.= $c;
-			} elsif ($o < 0xFFFF) {
-				$string	.= sprintf("\\u%04X", $o);
-			} else {
-				$string	.= sprintf("\\U%08X", $o);
-			}
-		}
-		my $ntriples	= '<' . $string . '>';
-		return $ntriples;
 	}
 }
 
