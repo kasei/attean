8c1b3ca7413534607f61ccdcbf2ae17498a2ca25
Gregory Todd Williams <greg@evilfunhouse.com>
2014-09-21 12:37:04 -0700

    Allow bin/attean_transcode.pl to print simple string serializations when no output format is specified.

--- bin/attean_transcode.pl
+++ bin/attean_transcode.pl
@@ -1,5 +1,7 @@
 #!/usr/bin/perl
 
+use v5.14;
+use autodie;
 use strict;
 use warnings;
 use Getopt::Long;
@@ -20,7 +22,7 @@ my $push		= 0;
 my $block_size	= 25;
 my %namespaces;
 my $in	= 'ntriples';
-my $out	= 'rdfxml';
+my $out	= 'string';
 my $result	= GetOptions ("verbose" => \$verbose, "block=i" => \$block_size, "pull" => \$pull, "push" => \$push, "in=s" => \$in, "out=s" => \$out, "define=s" => \%namespaces, "D=s" => \%namespaces);
 
 unless (@_) { push(@ARGV, '-') }
@@ -29,35 +31,46 @@ my $fh;
 if ($file eq '-') {
 	$fh	= \*STDIN;
 } else {
-	open( my $fh, '<:encoding(UTF-8)', $file ) or die $!;
+	open( $fh, '<:encoding(UTF-8)', $file ) or die $!;
 }
 
-my $done :shared;
-my $st :shared;
-
-$done			= 0;
-my $parser		= Attean->get_parser($in)->new();
-my $serializer	= Attean->get_serializer($out)->new(namespaces => \%namespaces);
 my $out_io		= \*STDOUT;
 $|				= 1;
+my $parser		= Attean->get_parser($in)->new();
+if ($out eq 'string') {
+	parse_to_string($parser, $fh);
+} else {
+	my $serializer	= Attean->get_serializer($out)->new(namespaces => \%namespaces);
 
-if ($pull) {
-	print "# Pull parsing\n" if ($verbose);
-	pull_transcode($parser, $serializer, $out_io);
-} elsif ($push) {
-	print "# Push parsing\n" if ($verbose);
-	push_transcode($parser, $serializer, $out_io);
-} elsif ($parser->does('Attean::API::PullParser')) {
-	print "# Pull parsing\n" if ($verbose);
-	pull_transcode($parser, $serializer, $out_io);
-} elsif ($parser->does('Attean::API::PushParser')) {
-	print "# Push parsing\n" if ($verbose);
-	push_transcode($parser, $serializer, $out_io);
+	if ($pull) {
+		print "# Pull parsing\n" if ($verbose);
+		pull_transcode($parser, $serializer, $fh, $out_io);
+	} elsif ($push) {
+		print "# Push parsing\n" if ($verbose);
+		push_transcode($parser, $serializer, $fh, $out_io);
+	} elsif ($parser->does('Attean::API::PullParser')) {
+		print "# Pull parsing\n" if ($verbose);
+		pull_transcode($parser, $serializer, $fh, $out_io);
+	} elsif ($parser->does('Attean::API::PushParser')) {
+		print "# Push parsing\n" if ($verbose);
+		push_transcode($parser, $serializer, $fh, $out_io);
+	}
+}
+
+sub parse_to_string {
+	my $parser		= shift;
+	my $fh			= shift;
+	$parser->handler(sub {
+		my $item	= shift;
+		say $item->as_string;
+	});
+	$parser->parse_cb_from_io($fh);
 }
 
 sub pull_transcode {
 	my $parser		= shift;
 	my $serializer	= shift;
+	my $fh			= shift;
 	my $out_io		= shift;
 	warn "Pull parser";
 	my $iter		= $parser->parse_iter_from_io($fh);
@@ -67,6 +80,7 @@ sub pull_transcode {
 sub push_transcode {
 	my $parser		= shift;
 	my $serializer	= shift;
+	my $fh			= shift;
 	my $out_io		= shift;
 	warn "Push parser";
 	if ($serializer->does('Attean::API::AppendableSerializer')) {
