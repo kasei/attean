fcd83f17cdad1c70cd62ad7f85d7a5158615b62a
Gregory Todd Williams <greg@evilfunhouse.com>
2015-03-26 15:18:12 -0700

    Added Test::Attean::MutableTripleStore.

---
+++ lib/Test/Attean/MutableTripleStore.pm
@@ -0,0 +1,42 @@
+package Test::Attean::MutableTripleStore;
+
+use v5.18;
+use Test::Roo::Role;
+use Test::Moose;
+use Attean;
+use Attean::RDF;
+
+requires 'create_store';       # create_store( triples => \@triples )
+
+test 'mutabletriplestore add_triple' => sub {
+    my $self	= shift;
+    my $t1		= triple(iri('s'), iri('p'), iri('o'));
+    my $t2		= triple(iri('x'), iri('y'), iri('z'));
+    my $t3		= triple(iri('x'), iri('y'), literal('123'));
+    
+    my $store	= $self->create_store(triples => []);
+    my $size	= 0;
+    for my $t ($t1, $t2, $t3) {
+    	$store->add_triple($t);
+    	is($store->size, ++$size, "size $size");
+    }
+};
+
+test 'mutabletriplestore remove_triple' => sub {
+    my $self	= shift;
+    my $t1		= triple(iri('s'), iri('p'), iri('o'));
+    my $t2		= triple(iri('x'), iri('y'), iri('z'));
+    my $t3		= triple(iri('x'), iri('y'), literal('123'));
+    
+    my $store	= $self->create_store(triples => [$t3, $t2, $t1]);
+    my $size	= 3;
+    for my $t ($t1, $t2, $t3) {
+    	is($store->size, $size, "size $size");
+    	$store->remove_triple($t);
+    	$size--;
+    }
+	$store->remove_triple($t2);
+	is($store->size, 0, "size $size");
+};
+
+1;
