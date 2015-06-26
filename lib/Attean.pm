65d9f5f533379edbfee9cacba48317ad43b4b7d4
Gregory Todd Williams <greg@evilfunhouse.com>
2015-06-25 23:46:01 -0400

    Updated VERSION and docs for 0.005_01 beta release.

--- lib/Attean.pm
+++ lib/Attean.pm
@@ -4,7 +4,7 @@ Attean - A Semantic Web Framework
 
 =head1 VERSION
 
-This document describes Attean version 0.005
+This document describes Attean version 0.005_01
 
 =head1 SYNOPSIS
 
@@ -41,7 +41,7 @@ Semantic Web (RDF and SPARQL) data.
 package Attean {
 	use v5.14;
 	use warnings;
-	our $VERSION	= '0.005';
+	our $VERSION	= '0.005_01';
 	use Attean::API;
 	
 	use Attean::Blank;
