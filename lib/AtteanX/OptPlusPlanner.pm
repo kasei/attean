863007b1312ed11a8a7c6f39d68677e91e4bb804
Gregory Todd Williams <greg@evilfunhouse.com>
2015-07-08 08:16:02 -0700

    Added filter evaluation inside OptPlus join plans.

--- lib/AtteanX/OptPlusPlanner.pm
+++ lib/AtteanX/OptPlusPlanner.pm
@@ -44,50 +44,49 @@ package AtteanX::OptPlusPlanner 0.005 {
 		if ($algebra->isa('Attean::Algebra::OptPlus')) {
 			my $active_graphs	= shift;
 			my $default_graphs	= shift;
-			my @children		= @{ $algebra->children };
+			
+			my ($la, $ra)		= @{ $algebra->children };
+			my $expr			= $algebra->expression;
+			
+			my @children		= ($la, $ra);
 			my @vars			= keys %{ { map { map { $_ => 1 } $_->in_scope_variables } @children } };
 			my @plansets		= map { [$self->plans_for_algebra($_, $model, $active_graphs, $default_graphs, @_)] } @children;
 			my $l				= [$self->plans_for_algebra($children[0], $model, $active_graphs, $default_graphs, @_)];
 			my $r				= [$self->plans_for_algebra($children[1], $model, $active_graphs, $default_graphs, @_)];
-			return $self->join_plans($model, $active_graphs, $default_graphs, $l, $r, 'optplus');
+			return $self->optplus_join_plans($model, $active_graphs, $default_graphs, $l, $r, $expr);
 		}
 		return $orig->(@args);
 	};
 	
-	around 'join_plans' => sub {
-		my $orig			= shift;
-		my @args			= @_;
+	sub optplus_join_plans {
 		my $self			= shift;
+		my @args			= @_;
 		my $model			= shift;
 		my $active_graphs	= shift;
 		my $default_graphs	= shift;
 		my $lplans			= shift;
 		my $rplans			= shift;
-		my $type			= shift;
-		if ($type eq 'optplus') {
-			my @plans;
-			foreach my $lhs (@{ $lplans }) {
-				foreach my $rhs (@{ $rplans }) {
-					my @vars	= (@{ $lhs->in_scope_variables }, @{ $rhs->in_scope_variables });
-					my %vars;
-					my %join_vars;
-					foreach my $v (@vars) {
-						if ($vars{$v}++) {
-							$join_vars{$v}++;
-						}
+		my $expr			= shift;
+		my @plans;
+		foreach my $lhs (@{ $lplans }) {
+			foreach my $rhs (@{ $rplans }) {
+				my @vars	= (@{ $lhs->in_scope_variables }, @{ $rhs->in_scope_variables });
+				my %vars;
+				my %join_vars;
+				foreach my $v (@vars) {
+					if ($vars{$v}++) {
+						$join_vars{$v}++;
 					}
-					my @join_vars	= keys %join_vars;
-					push(@plans, AtteanX::OptPlusPlanner::NestedLoopJoin->new(children => [$lhs, $rhs], left => 1, join_variables => \@join_vars, distinct => 0, in_scope_variables => [keys %vars], ordered => []));
-					if (scalar(@join_vars) > 0) {
-						push(@plans, AtteanX::OptPlusPlanner::HashJoin->new(children => [$lhs, $rhs], join_variables => \@join_vars, distinct => 0, in_scope_variables => [keys %vars], ordered => []));
-					}
-					
 				}
+				my @join_vars	= keys %join_vars;
+				push(@plans, AtteanX::OptPlusPlanner::NestedLoopJoin->new(children => [$lhs, $rhs], expression => $expr, left => 1, join_variables => \@join_vars, distinct => 0, in_scope_variables => [keys %vars], ordered => [] ));
+				if (scalar(@join_vars) > 0) {
+					push(@plans, AtteanX::OptPlusPlanner::HashJoin->new(children => [$lhs, $rhs], expression => $expr, join_variables => \@join_vars, distinct => 0, in_scope_variables => [keys %vars], ordered => []));
+				}
+				
 			}
-			return @plans;
 		}
-		
-		return $orig->(@args);
+		return @plans;
 	};
 	
 	
@@ -95,13 +94,22 @@ package AtteanX::OptPlusPlanner 0.005 {
 
 package AtteanX::OptPlusPlanner::NestedLoopJoin 0.005 {
 	use Moo;
+	use Scalar::Util qw(blessed);
 	use Types::Standard qw(ArrayRef Str Bool ConsumerOf);
+	use namespace::clean;
+
 	with 'Attean::API::Plan', 'Attean::API::BinaryQueryTree';
 	has 'join_variables' => (is => 'ro', isa => ArrayRef[Str], required => 1);
-	sub plan_as_string { return 'NestedLoop OptPlusJoin' }
-	sub impl {
+	has 'expression' => (is => 'ro', isa => ConsumerOf['Attean::API::Expression'], required => 0, default => sub { Attean::ValueExpression->new( value => Attean::Literal->true ) });
+	sub plan_as_string {
 		my $self	= shift;
-		my $model	= shift;
+		my $expr	= $self->expression;
+		return sprintf('NestedLoop OptPlusJoin { %s }', $expr->as_sparql);
+	}
+	sub impl {
+		my $self		= shift;
+		my $model		= shift;
+		my $expr		= $self->expression;
 		my @children	= map { $_->impl($model) } @{ $self->children };
 		return sub {
 			my ($lhs, $rhs)	= map { $_->() } @children;
@@ -111,7 +119,10 @@ package AtteanX::OptPlusPlanner::NestedLoopJoin 0.005 {
 				push(@results, $l);
 				foreach my $r (@right) {
 					if (my $j = $l->join($r)) {
-						push(@results, $j);
+						my $val	= Attean::Plan::Extend->evaluate_expression($model, $expr, $j);
+						if (blessed($val) and $val->ebv) {
+							push(@results, $j);
+						}
 					}
 				}
 			}
@@ -128,14 +139,17 @@ package AtteanX::OptPlusPlanner::HashJoin 0.005 {
 	use Types::Standard qw(ArrayRef Str ConsumerOf Bool);
 	with 'Attean::API::Plan', 'Attean::API::BinaryQueryTree';
 	has 'join_variables' => (is => 'ro', isa => ArrayRef[Str], required => 1);
+	has 'expression' => (is => 'ro', isa => ConsumerOf['Attean::API::Expression'], required => 0, default => sub { Attean::ValueExpression->new( value => Attean::Literal->true ) });
 	sub plan_as_string {
 		my $self	= shift;
-		return sprintf('NestedLoop OptPlusJoin { %s }', join(', ', @{$self->join_variables}));
+		my $expr	= $self->expression;
+		return sprintf('NestedLoop OptPlusJoin { join=%s, filter=%s }', join(', ', @{$self->join_variables}), $expr->as_sparql);
 	}
 
 	sub impl {
 		my $self	= shift;
 		my $model	= shift;
+		my $expr		= $self->expression;
 		my @children	= map { $_->impl($model) } @{ $self->children };
 		return sub {
 			my %hash;
@@ -154,7 +168,10 @@ package AtteanX::OptPlusPlanner::HashJoin 0.005 {
 				if (my $rows = $hash{$key}) {
 					foreach my $r (@$rows) {
 						if (my $j = $l->join($r)) {
-							push(@results, $j);
+							my $val	= Attean::Plan::Extend->evaluate_expression($model, $expr, $j);
+							if (blessed($val) and $val->ebv) {
+								push(@results, $j);
+							}
 						}
 					}
 				}
