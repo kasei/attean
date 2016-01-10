7e6e2c166d9798552ac1dd921487058965f7adf3
Gregory Todd Williams <greg@evilfunhouse.com>
2016-01-10 11:04:44 -0800

    Fix token type used in Attean::API::Blank->sparql_tokens.

--- lib/Attean/API/Term.pm
+++ lib/Attean/API/Term.pm
@@ -493,7 +493,7 @@ package Attean::API::Blank 0.010 {
 	
 	sub sparql_tokens {
 		my $self	= shift;
-		my $t	= AtteanX::SPARQL::Token->fast_constructor( ANON, -1, -1, -1, -1, [$self->value] );
+		my $t	= AtteanX::SPARQL::Token->fast_constructor( BNODE, -1, -1, -1, -1, [$self->value] );
 		return Attean::ListIterator->new( values => [$t], item_type => 'AtteanX::SPARQL::Token' );
 	}
 	
