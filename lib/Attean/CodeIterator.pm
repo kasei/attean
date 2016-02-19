ea9070edab528eb99ed5681e5c11db74af0e6d61
Gregory Todd Williams <greg@evilfunhouse.com>
2016-02-19 13:33:07 -0800

    Improve error message in Attean::CodeIterator.

--- lib/Attean/CodeIterator.pm
+++ lib/Attean/CodeIterator.pm
@@ -84,7 +84,7 @@ Returns the iterator's next item, or undef upon reaching the end of iteration.
 		}
 		my $role	= $self->item_type;
 		if (Role::Tiny->is_role($role)) {
-			die "CodeIterator item is not a $role" unless (blessed($item) and $item->does($role));
+			die "CodeIterator item is not a $role: $item" unless (blessed($item) and $item->does($role));
 		}
 		return $item;
 	}
