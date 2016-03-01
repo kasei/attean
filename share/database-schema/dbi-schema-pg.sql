e733f27cc3f1cef3d820210b4b716424d69b6a8d
Gregory Todd Williams <greg@evilfunhouse.com>
2016-03-01 09:22:29 -0800

    Added database schema files.

---
+++ share/database-schema/dbi-schema-pg.sql
@@ -0,0 +1,24 @@
+DROP TABLE quad;
+DROP TABLE term;
+DROP TYPE TERM_TYPE;
+
+CREATE TYPE TERM_TYPE AS ENUM ('iri', 'literal', 'blank');
+CREATE TABLE IF NOT EXISTS term (
+	term_id SERIAL PRIMARY KEY,
+	type TERM_TYPE,
+	datatype_id BIGINT REFERENCES term(term_id),
+	value TEXT NOT NULL,
+	language TEXT,
+	UNIQUE (datatype_id, value, language)
+);
+
+CREATE TABLE IF NOT EXISTS quad (
+	subject BIGINT NOT NULL REFERENCES term,
+	predicate BIGINT NOT NULL REFERENCES term,
+	object BIGINT NOT NULL REFERENCES term,
+	graph BIGINT NOT NULL REFERENCES term,
+	PRIMARY KEY (subject, predicate, object, graph)
+);
+
+CREATE INDEX ON quad (graph);
+CREATE INDEX ON quad (predicate,object,graph,subject);
