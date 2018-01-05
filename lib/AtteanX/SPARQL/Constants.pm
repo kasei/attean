6ceb1cf1a1c5528898a650597dcd3c20aff35b47
Gregory Todd Williams <greg@evilfunhouse.com>
2018-01-05 09:30:56 -0800

    Removed LICENSE file and updated licensing statement in individual modules (#116).

--- lib/AtteanX/SPARQL/Constants.pm
+++ lib/AtteanX/SPARQL/Constants.pm
@@ -103,7 +103,7 @@ Gregory Todd Williams  C<< <gwilliams@cpan.org> >>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2014--2016 Toby Inkster. This
+Copyright (c) 2014--2018 Toby Inkster. This
 program is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
 
--- lib/AtteanX/Serializer/NTriples.pm
+++ lib/AtteanX/Serializer/NTriples.pm
@@ -83,7 +83,7 @@ Gregory Todd Williams  C<< <gwilliams@cpan.org> >>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2014--2016 Gregory Todd Williams. This
+Copyright (c) 2014--2018 Gregory Todd Williams. This
 program is free software; you can redistribute it and/or modify it under
 the same terms as Perl itself.
 
